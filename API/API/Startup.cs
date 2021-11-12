6cd43ef60b7110a09c16a4944f29f1962de99777
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-11-12 14:23:03 +0200

    Adde Cors policy

--- API/API/Startup.cs
+++ API/API/Startup.cs
@@ -25,6 +25,7 @@ namespace API
             });
             services.AddDbContext<StoreContext>(options =>
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +41,13 @@ namespace API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(opt => 
+            {
+                opt.AllowAnyHeader()
+                   .AllowAnyMethod()
+                   .WithOrigins("http://localhost:3000");
+            });
 
             app.UseAuthorization();
 
