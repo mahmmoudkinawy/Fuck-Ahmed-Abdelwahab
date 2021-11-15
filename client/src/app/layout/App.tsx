24636d868fe895847ea5f966bbc172ec40c3c87d
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-11-15 19:22:22 +0200

    Refactoring

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,40 +1,14 @@
 import { Container, CssBaseline } from "@mui/material";
-import { useEffect, useState } from "react";
 import Catalog from "../../features/catalog/catalog";
-import { Product } from "../models/product";
 import Header from "./Header";
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
-
-  useEffect(() => {
-    fetch("http://localhost:5000/api/products")
-      .then((response) => response.json())
-      .then((data) => setProducts(data));
-  }, []);
-
-  function addProduct() {
-    setProducts((preState) => [
-      ...preState,
-      {
-        id: preState.length + 101,
-        name: "product" + (preState.length + 1),
-        price: preState.length * 100 + 100,
-        brand: "some brand",
-        description: "some description",
-        pictureUrl: "http://picsum.photos/200",
-        // quantityInStock: 100,
-        // type: "some type",
-      },
-    ]);
-  }
-
   return (
     <>
       <CssBaseline />
       <Header />
       <Container>
-        <Catalog products={products} addProduct={addProduct} />
+        <Catalog  />
       </Container>
     </>
   );
