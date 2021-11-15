eb280ace945abb7f01f777d2adcd2f98e5d7400b
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-11-15 19:19:22 +0200

    Added card and it's details

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,13 +1,11 @@
 import { AppBar, Toolbar, Typography } from "@mui/material";
 
 export default function Header() {
-    return (
-        <AppBar position='static'>
-            <Toolbar>
-                <Typography variant='h6'>
-                    JULIA
-                </Typography>
-            </Toolbar>
-        </AppBar>
-    )
+  return (
+    <AppBar position="static" sx={{ mb: 4 }}>
+      <Toolbar>
+        <Typography variant="h6">JULIA</Typography>
+      </Toolbar>
+    </AppBar>
+  );
 }
--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -1,17 +1,19 @@
-import { List } from "@mui/material";
+import { Grid } from "@mui/material";
 import { Product } from "../../app/models/product";
 import ProductCard from "./ProductCard";
 
 interface Props {
-    products: Product[];
+  products: Product[];
 }
 
-export default function ProductList({products}: Props) {
-    return (
-        <List>
-            {products.map(product => (
-                <ProductCard key={product.id} product={product} />
-            ))}
-      </List>
-    )
+export default function ProductList({ products }: Props) {
+  return (
+    <Grid container spacing={4}>
+      {products.map((product) => (
+        <Grid item xs={3} key={product.id}>
+          <ProductCard product={product} />
+        </Grid>
+      ))}
+    </Grid>
+  );
 }
