f1af12bcfc947518afbd836085c69813f0c173b3
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-11-15 19:36:37 +0200

    Added toggle for theme color

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,16 +1,34 @@
-import { Container, CssBaseline } from "@mui/material";
+import {
+  Container,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+} from "@mui/material";
+import { useState } from "react";
 import Catalog from "../../features/catalog/catalog";
 import Header from "./Header";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? "dark" : "light";
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+    },
+  });
+
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Header />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Catalog  />
+        <Catalog />
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
 
