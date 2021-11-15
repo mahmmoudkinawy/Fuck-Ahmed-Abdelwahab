0ceb5fce6a587a7fbde3223b5b1d0d6b569c6f67
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-11-15 19:41:30 +0200

    Fixed background color with dark mode

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -14,6 +14,9 @@ function App() {
   const theme = createTheme({
     palette: {
       mode: paletteType,
+      background: {
+        default: paletteType === "light" ? "#eaeaea" : "#121212",
+      },
     },
   });
 
