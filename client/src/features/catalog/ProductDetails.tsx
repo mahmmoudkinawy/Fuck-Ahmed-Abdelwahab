e49c4175ec9b2e99d9f0661023e1b76583824da9
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-11-16 16:06:57 +0200

    Added axios requests for errors

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -8,9 +8,9 @@ import {
   TableRow,
   Typography,
 } from "@mui/material";
-import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import agent from "../../app/api/agent";
 import { Product } from "../../app/models/product";
 
 export default function ProductDetails() {
@@ -19,10 +19,9 @@ export default function ProductDetails() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios
-      .get(`http://localhost:5000/api/products/${id}`)
-      .then((response) => setProduct(response.data))
-      .catch((error) => console.log(error))
+    agent.Catalog.details(Number(id))
+      .then((response) => setProduct(response))
+      .catch((error) => console.log(error.response))
       .finally(() => setLoading(false));
   }, [id]);
 
