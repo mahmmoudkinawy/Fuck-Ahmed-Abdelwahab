9a08269ec40ed85b556d8914c2e2f22f225c48d0
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-11-17 19:02:12 +0200

    Updated product details and added not found component

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -13,8 +13,9 @@ import AboutPage from "../../features/about/AboutPage";
 import ContactPage from "../../features/contact/ContactPage";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import ServerError from "../errors/ServerError";
+import NotFound from "../errors/NotFound";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -38,12 +39,15 @@ function App() {
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/catalog" exact component={Catalog} />
-        <Route path="/catalog/:id" component={ProductDetails} />
-        <Route path="/about" component={AboutPage} />
-        <Route path="/contact" component={ContactPage} />
-        <Route path="/server-error" component={ServerError} />
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/catalog" exact component={Catalog} />
+          <Route path="/catalog/:id" component={ProductDetails} />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/contact" component={ContactPage} />
+          <Route path="/server-error" component={ServerError} />
+          <Route component={NotFound} />
+        </Switch>
       </Container>
     </ThemeProvider>
   );
