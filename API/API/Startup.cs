9aa8faafcc300bceb20fe1f7cf090d5d95c26790
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-12-06 21:18:49 +0200

    Updated CORS to care about cookies in front end

--- API/API/Startup.cs
+++ API/API/Startup.cs
@@ -49,6 +49,7 @@ namespace API
             {
                 opt.AllowAnyHeader()
                    .AllowAnyMethod()
+                   .AllowCredentials()
                    .WithOrigins("http://localhost:3000");
             });
 
