52326bf73e2b920392f10db7f046269cabef1f4b
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-12-06 22:07:13 +0200

    Added basket, Interface for it and requests from API for it.

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -16,6 +16,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { Route, Switch } from "react-router-dom";
 import ServerError from "../errors/ServerError";
 import NotFound from "../errors/NotFound";
+import BasketPage from "../../features/basket/BasketPage";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -46,6 +47,7 @@ function App() {
           <Route path="/about" component={AboutPage} />
           <Route path="/contact" component={ContactPage} />
           <Route path="/server-error" component={ServerError} />
+          <Route path="/basket" component={BasketPage} />
           <Route component={NotFound} />
         </Switch>
       </Container>
