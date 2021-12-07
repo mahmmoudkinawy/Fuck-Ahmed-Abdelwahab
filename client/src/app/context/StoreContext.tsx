f4b4b6108de5a8ef5e812dc29a681b63674f5e03
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-12-07 14:55:00 +0200

    Added get data from API via context

--- client/src/app/context/StoreContext.tsx
+++ client/src/app/context/StoreContext.tsx
@@ -22,24 +22,24 @@ export function useStoreContext() {
 }
 
 export function StoreProvider({ children }: PropsWithChildren<any>) {
-    const [basket, setBasket] = useState<Basket | null>(null);
+  const [basket, setBasket] = useState<Basket | null>(null);
 
-    function removeItem(productId: number, quantity: number) {
-        if(!basket) return;
-        const items = [...basket.items];
-        const itemIndex = items.findIndex(i => i.productId === productId);
-        if(itemIndex >= 0) {
-            items[itemIndex].quantity -= quantity;
-            if(items[itemIndex].quantity === 0) items.splice(itemIndex, 1);
-            setBasket(prevState => {
-                return {...prevState!, items};
-            })
-        }
+  function removeItem(productId: number, quantity: number) {
+    if (!basket) return;
+    const items = [...basket.items];
+    const itemIndex = items.findIndex((i) => i.productId === productId);
+    if (itemIndex >= 0) {
+      items[itemIndex].quantity -= quantity;
+      if (items[itemIndex].quantity === 0) items.splice(itemIndex, 1);
+      setBasket((prevState) => {
+        return { ...prevState!, items };
+      });
     }
+  }
 
-    return (
-        <StoreContext.Provider value={{basket, setBasket, removeItem}}>
-            {children}
-        </StoreContext.Provider>
-    )
+  return (
+    <StoreContext.Provider value={{ basket, setBasket, removeItem }}>
+      {children}
+    </StoreContext.Provider>
+  );
 }
--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -4,7 +4,7 @@ import {
   CssBaseline,
   ThemeProvider,
 } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Catalog from "../../features/catalog/catalog";
 import Header from "./Header";
 import HomePage from "../../features/home/HomePage";
@@ -17,8 +17,25 @@ import { Route, Switch } from "react-router-dom";
 import ServerError from "../errors/ServerError";
 import NotFound from "../errors/NotFound";
 import BasketPage from "../../features/basket/BasketPage";
+import { useStoreContext } from "../context/StoreContext";
+import { getCookie } from "../util/util";
+import agent from "../api/agent";
+import LoadingComponent from "./LoadingComponent";
 
 function App() {
+  const { setBasket } = useStoreContext();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const buyerId = getCookie("buyerId");
+    if (buyerId) {
+      agent.Basket.get()
+        .then((basket) => setBasket(basket))
+        .catch((error) => console.log(error))
+        .finally(() => setLoading(false))
+    }
+  }, [setBasket]);
+
   const [darkMode, setDarkMode] = useState(false);
   const paletteType = darkMode ? "dark" : "light";
   const theme = createTheme({
@@ -33,6 +50,8 @@ function App() {
   function handleThemeChange() {
     setDarkMode(!darkMode);
   }
+
+  if(loading) return <LoadingComponent message='Initialising app...' />
 
   return (
     <ThemeProvider theme={theme}>
