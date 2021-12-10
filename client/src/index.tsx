6ab4437986c7d736c6f9a51772ed7b5ad3e0726a
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-12-10 14:18:33 +0200

    Added redux tool kit and configure it

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,6 +6,8 @@ import reportWebVitals from "./reportWebVitals";
 import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
 import { StoreProvider } from "./app/context/StoreContext";
+import { Provider } from "react-redux";
+import { store } from "./app/store/configureStore";
 
 export const history = createBrowserHistory();
 
@@ -13,7 +15,9 @@ ReactDOM.render(
   <React.StrictMode>
     <Router history={history}>
       <StoreProvider>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </StoreProvider>
     </Router>
   </React.StrictMode>,
