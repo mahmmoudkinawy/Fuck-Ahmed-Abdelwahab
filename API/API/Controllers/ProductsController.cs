bbe931e953c886ec68b0102b4f4205e3a5a5acab
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-12-16 15:50:11 +0200

    Added end point for getting types and brands.

--- API/API/Controllers/ProductsController.cs
+++ API/API/Controllers/ProductsController.cs
@@ -42,5 +42,15 @@ namespace API.Controllers
 
             return product == null ? NotFound() : Ok(product);
         }
+
+        [HttpGet("filters")]
+        public async Task<IActionResult> GetFilters()
+        {
+            var brands = await _context.Products.Select(p => p.Brand).Distinct().ToListAsync();
+            var types = await _context.Products.Select(p => p.Type).Distinct().ToListAsync();
+
+            return Ok(new { brands, types });
+        }
+
     }
 }
