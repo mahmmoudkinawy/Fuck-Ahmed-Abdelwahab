a73b8368231b94f07e6f03698d2253178a992e16
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-12-20 16:50:03 +0200

    Added tamplate for login

--- client/src/features/account/Login.tsx
+++ client/src/features/account/Login.tsx
@@ -10,19 +10,26 @@ import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Paper } from "@mui/material";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import agent from "../../app/api/agent";
 
 const theme = createTheme();
 
 export default function Login() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const [values, setValues] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleSubmit = (event: any) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    // eslint-disable-next-line no-console
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+    agent.Account.login(values);
   };
+
+  function handleInputChange(event: any) {
+    const { name, value } = event.target;
+    setValues({ ...values, [name]: value });
+  }
 
   return (
     <ThemeProvider theme={theme}>
@@ -45,23 +52,21 @@ export default function Login() {
         <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
           <TextField
             margin="normal"
-            required
             fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
+            label="Username"
+            name="username"
             autoFocus
+            onChange={handleInputChange}
+            value={values.username}
           />
           <TextField
             margin="normal"
-            required
             fullWidth
             name="password"
             label="Password"
             type="password"
-            id="password"
-            autoComplete="current-password"
+            onChange={handleInputChange}
+            value={values.password}
           />
           <Button
             type="submit"
