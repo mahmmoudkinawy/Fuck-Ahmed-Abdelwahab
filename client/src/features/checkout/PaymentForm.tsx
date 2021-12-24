17cc2898bf3ef7f4555b7e07c5fa839d38c4186a
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-12-24 13:42:09 +0200

    Added validation to stripe inputs but got an bug.

--- client/src/features/checkout/PaymentForm.tsx
+++ client/src/features/checkout/PaymentForm.tsx
@@ -10,9 +10,31 @@ import {
   CardNumberElement,
 } from "@stripe/react-stripe-js";
 import { StripeInput } from "./StripeInput";
+import { useState } from "react";
+import { StripeElementType } from "@stripe/stripe-js";
 
 export default function PaymentForm() {
   const { control } = useFormContext();
+  const [cardState, setCardState] = useState<{
+    elementError: { [key in StripeElementType]?: string };
+  }>({ elementError: {} });
+  const [cardComplete, setCardComplete] = useState<any>({
+    cardNumber: false,
+    cardExpiry: false,
+    cardCvc: false,
+  });
+
+  function onCardInputChange(event: any) {
+    setCardState({
+      ...cardState,
+      elementError: {
+        ...cardState.elementError,
+        [event.elementType]: event.error?.message,
+      },
+    });
+    setCardComplete({ ...cardComplete, [event.elementType]: event.complete });
+  }
+
   return (
     <>
       <Typography variant="h6" gutterBottom>
@@ -28,6 +50,9 @@ export default function PaymentForm() {
         </Grid>
         <Grid item xs={12} md={6}>
           <TextField
+            onChange={onCardInputChange}
+            error={!!cardState.elementError.cardNumber}
+            helperText={cardState.elementError.cardNumber}
             id="cardNumber"
             label="Card number"
             fullWidth
@@ -44,6 +69,9 @@ export default function PaymentForm() {
         </Grid>
         <Grid item xs={12} md={6}>
           <TextField
+            onChange={onCardInputChange}
+            error={!!cardState.elementError.cardExpiry}
+            helperText={cardState.elementError.cardExpiry}
             id="expDate"
             label="Expiry date"
             fullWidth
@@ -60,9 +88,11 @@ export default function PaymentForm() {
         </Grid>
         <Grid item xs={12} md={6}>
           <TextField
+            onChange={onCardInputChange}
+            error={!!cardState.elementError.cardCvc}
+            helperText={cardState.elementError.cardCvc}
             id="cvv"
             label="CVV"
-            helperText="Last three digits on signature strip"
             fullWidth
             autoComplete="cc-csc"
             variant="outlined"
