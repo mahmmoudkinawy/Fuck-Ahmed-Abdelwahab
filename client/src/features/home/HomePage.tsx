aa68ce93a31a3d70d613fdd390a7e29e64098d80
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-12-24 16:18:12 +0200

    Added home page basic styles

--- client/src/features/home/HomePage.tsx
+++ client/src/features/home/HomePage.tsx
@@ -1,5 +1,42 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import Slider from "react-slick";
 
 export default function HomePage() {
-  return <Typography variant="h2">Home Page</Typography>;
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  return (
+    <>
+      <Slider {...settings}>
+        <div>
+          <img
+            src="https://images.unsplash.com/photo-1567401893414-76b7b1e5a7a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt=""
+            style={{ display: "block", width: "100%", maxHeight: 600 }}
+          />
+        </div>
+        <div>
+          <img
+            src="https://images.unsplash.com/photo-1567401893414-76b7b1e5a7a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt=""
+            style={{ display: "block", width: "100%", maxHeight: 600 }}
+          />
+        </div>
+        <div>
+          <img
+            src="https://images.unsplash.com/photo-1567401893414-76b7b1e5a7a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt=""
+            style={{ display: "block", width: "100%", maxHeight: 600 }}
+          />
+        </div>
+      </Slider>
+      <Box display="flex" justifyContent="center" sx={{ p: 4 }}>
+        <Typography>Welcome to the shop!</Typography>
+      </Box>
+    </>
+  );
 }
