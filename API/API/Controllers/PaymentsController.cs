c7fb521d4cf760b907f85a9fd5c86e0f4d075ccd
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-12-24 15:58:09 +0200

    Added web hoot to the api

--- API/API/Controllers/PaymentsController.cs
+++ API/API/Controllers/PaymentsController.cs
@@ -1,10 +1,14 @@
 ﻿using API.Data;
 using API.DTOs;
+using API.Entities.OrderAggregate;
 using API.Extensions;
 using API.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Stripe;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace API.Controllers
@@ -13,11 +17,13 @@ namespace API.Controllers
     {
         private readonly PaymentService _paymentService;
         private readonly StoreContext _context;
+        private readonly IConfiguration _config;
 
-        public PaymentsController(PaymentService paymentService, StoreContext context)
+        public PaymentsController(PaymentService paymentService, StoreContext context, IConfiguration config)
         {
             _paymentService = paymentService;
             _context = context;
+            _config = config;
         }
 
         [Authorize]
@@ -46,6 +52,26 @@ namespace API.Controllers
                 return BadRequest(new ProblemDetails { Title = "Problem updating basket with intent" });
 
             return basket.MapBasketToDto();
+        }
+
+        [HttpPost("webhook")]
+        public async Task<ActionResult> StripeWebhook()
+        {
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+
+            var stripeEvent = EventUtility.ConstructEvent(json, Request.Headers["Stripe-Signature"],
+                _config["StripeSettings:WhSecret"]);
+
+            var charge = (Charge)stripeEvent.Data.Object;
+
+            var order = await _context.Orders.FirstOrDefaultAsync(x =>
+                x.PaymentIntentId == charge.PaymentIntentId);
+
+            if (charge.Status == "succeeded") order.OrderStatus = OrderStatus.PaymentReceived;
+
+            await _context.SaveChangesAsync();
+
+            return new EmptyResult();
         }
     }
 }
